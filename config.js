b2386839222ac7c560349467ed0d52f0347654cf
sokhakun <77477088+sokhakun@users.noreply.github.com>
2021-07-12 01:36:08 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '175126137998296',
-        clientSecret: '00c9e732f0d05dce4b513216dfa4a4d8'
-    }
-}
